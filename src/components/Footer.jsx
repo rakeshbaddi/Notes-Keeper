27db10633b6806b867e840ce7f770bdc598d9f69
Rakesh Baddi <you@example.com>
2021-05-16 21:10:17 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <p>Created with ❤️ by Rakesh Baddi</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostAddIcon from '@material-ui/icons/PostAdd';
+
+function Header() {
+    return (
+        <header>
+            <h1><PostAddIcon />  Notes Keeper</h1>
+        </header>
+    );
+}
+
+export default Header;
